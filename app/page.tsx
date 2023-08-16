d54361154a1c7743ddc3b8dcb92524c75e6f721b
PuniaMohit <mohitpunia@innotechteam.in>
2023-08-16 08:36:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Banner from "./components/Banner/banner";
+import Content from "./components/Content/content";
+
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      <Content />
+    </>
+  );
+}
